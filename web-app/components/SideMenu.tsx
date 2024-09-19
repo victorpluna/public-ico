6b458c233e821e530202e6775921aa7dcd978f12
Victor Luna <victorluna22@gmail.com>
2024-09-19 15:42:01 -0300

    Add logo in the side menu component

--- web-app/components/SideMenu.tsx
+++ web-app/components/SideMenu.tsx
@@ -1,18 +1,24 @@
 "use client";
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import { Box, Center, VStack } from "@chakra-ui/react";
 import { FaLayerGroup } from "react-icons/fa6";
 
 import SideMenuItem from "./SideMenuItem";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function SideMenu() {
   const currentPath = usePathname();
 
   return (
     <Box as="nav" w="250px" p={4} position="fixed" h="100vh">
-      <Heading as="h1" size="lg" mb="40px">
-        Public ICO
-      </Heading>
+      <Center mb="40px">
+        <Image
+          alt="dico logo"
+          src="/images/logo-light.png"
+          width="150"
+          height="50"
+        />
+      </Center>
       <VStack align="start" spacing={5}>
         <SideMenuItem
           icon={FaLayerGroup}
