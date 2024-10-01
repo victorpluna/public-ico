a59a14d444c199054f59a30555e075c89bb7853c
Victor Luna <victorluna22@gmail.com>
2024-10-01 20:05:38 -0300

    Add some contributions to the deploy script

--- runner/ignition/scripts/deploy.ts
+++ runner/ignition/scripts/deploy.ts
@@ -18,6 +18,9 @@ async function main() {
     { value: 5000000000000000000n }
   );
 
+  await contract.contribute(0, { value: 1000000000000000000n });
+  await contract.contribute(0, { value: 2000000000000000000n });
+
   await contract.createProject(
     "Test Mocked Project 2",
     "https://mag.wcoomd.org/uploads/2018/05/blank.pdf",
@@ -27,6 +30,8 @@ async function main() {
     "0xfC95F24cf2Dc7205670E4Eb73aC7F743Add0bf57",
     { value: 12000000000000000000n }
   );
+
+  await contract.contribute(1, { value: 1000000000000000000n });
 
   await contract.createProject(
     "Test Mocked Project 3",
